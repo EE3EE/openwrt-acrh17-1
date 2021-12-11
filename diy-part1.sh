47dc412374e77bf865531ac8dffd4e2a4d0f0d85
jmingzhan <58477326+jmingzhan@users.noreply.github.com>
2021-12-11 15:41:06 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,4 +15,5 @@
 
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+echo 'src-git wolplus https://github.com/siwind/luci-app-wolplus' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
